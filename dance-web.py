60b4b42abce0517130fb07fc0b429a88932b15f7
vagrant <vagrant@vagrant.vm>
2020-10-22 06:27:15 +0000

    added app run when program ran

--- dance-web.py
+++ dance-web.py
@@ -27,3 +27,22 @@ FEEL_GOODS = {
         'name' : 'Jadah Sellner'
     },
 }
+
+@app.route('/')
+def go_to_homepage():
+    """directs to homepage"""
+
+    return render_template('homepage.html')
+
+
+
+
+
+if __name__ == '__main__':
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    DebugToolbarExtension(app)
+
+    app.run(host='0.0.0.0')
